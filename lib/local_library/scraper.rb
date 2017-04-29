ccb70f0330059696e2178a08ec2b90d03a9db9d7
Kyle Hughes <blinded93@gmail.com>
2017-04-29 00:23:46 +0000

    finished Scraper#scrape_and_create method.

--- lib/local_library/scraper.rb
+++ lib/local_library/scraper.rb
@@ -1,10 +1,16 @@
 class LocalLibrary::Scraper
 
-  def self.scrape(zip_code)
+  def self.scrape_and_create(zip_code)
     address = "https://search.yahoo.com/search?p=public+library+near+#{zip_code}"
     doc = Nokogiri::HTML(open(address))
     ul = doc.css('div.dd')
-    
+    ul.css('li.list').each do |a|
+      library = LocalLibrary::Library.new(a.css('div.titlewrapper a').text)
+      a.css('div.meta span.col.isopen').text == "Open" ? library.status = "Open" : library.status = "Closed"
+      library.address = a.css('div.contact span.addr').text
+      library.phone = a.css('div.contact span.hoo span')[1].text
+      LocalLibrary::Library.libraries << library
+    end
   end
 
 
