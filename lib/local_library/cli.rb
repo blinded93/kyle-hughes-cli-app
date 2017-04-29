759345877ebc110b0a37ffe8721c5f2a35709552
Kyle Hughes <blinded93@gmail.com>
2017-04-29 00:16:48 +0000

    started Scraper#scrape, CLI#call and CLI#start

--- lib/local_library/cli.rb
+++ lib/local_library/cli.rb
@@ -1,6 +1,18 @@
 class LocalLibrary::CLI
   def call
-
+    puts ""
+    puts "Welcome to the local library finder"
+    puts "Please enter a zip code to find the nearest library or 'exit' to exit program."
+    puts ""
+    start
   end
-  
+
+  def start
+    zip_code = nil
+    while zip_code != 'exit'
+      zip_code = gets.strip
+      if ZIPS.include?(zip_code)
+        library = LocalLibrary::Scraper
+  end
+
 end
--- lib/local_library/scraper.rb
+++ lib/local_library/scraper.rb
@@ -1,5 +1,11 @@
 class LocalLibrary::Scraper
 
+  def self.scrape(zip_code)
+    address = "https://search.yahoo.com/search?p=public+library+near+#{zip_code}"
+    doc = Nokogiri::HTML(open(address))
+    ul = doc.css('div.dd')
+    
+  end
 
 
 end
