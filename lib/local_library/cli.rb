459fb2aee1410266e46108e3aad33dfe17c536d8
Kyle Hughes <blinded93@gmail.com>
2017-04-29 02:24:21 +0000

    removed details from initial list, to add another layer of depth, per project instructions

--- lib/local_library/cli.rb
+++ lib/local_library/cli.rb
@@ -1,7 +1,8 @@
 class LocalLibrary::CLI
   def call
     puts ""
-    puts "Welcome to the local library finder"
+    puts "-------------------- Welcome to the Local library finder --------------------"
+    # puts ""
     puts "Please enter a zip code to find the nearest library or 'exit' to exit program."
     puts ""
     start
@@ -12,17 +13,21 @@ class LocalLibrary::CLI
     while zip_code != 'exit'
       zip_code = gets.strip
       if ZIPS.include?(zip_code)
-        puts "Public libraries near #{zip_code}"
+        puts ".....Public libraries near #{zip_code}....."
         library = LocalLibrary::Scraper.scrape_and_create(zip_code)
         LocalLibrary::Library.libraries.each.with_index(1) do |library, i|
           puts "#{i}. #{library.branch}"
-          puts "    #{library.phone}"
-          puts "    #{library.address}"
-          puts "    #{library.status}"
-          puts ""
+          # puts "    #{library.phone}"
+          # puts "    #{library.address}"
+          # puts "    #{library.status}"
+          # puts ""
         end
+        puts ""
+        puts "Please enter another zip code or 'exit' to exit program."
+        puts ""
+        LocalLibrary::Library.clear
       else
-        puts "That is not a valid zip code, please try again or type 'exit' to exit program."
+        puts "That is not a valid zip code, please try again, or type 'exit' to exit program."
       end
     end
   end
